4e8d796303362e8bd1a618547300297da0d2262e
migmatos <matosmoo2002@gmail.com>
2024-12-04 05:36:24 -0600

    0.1

--- main.py
+++ main.py
@@ -2,28 +2,66 @@ import tkinter as tk
 from tkinter import filedialog
 from mido import MidiFile
 from zlib import compress
-from base64 import urlsafe_b64encode
+from base64 import urlsafe_b64encode,b64encode
+import os
+
+def gdNumber(number:float) -> str:return "{:.5e}".format(number) if number > 9999 else str(number)
+def gdText(text:str) -> str: return b64encode(text.encode('utf-8')).decode('utf-8')
+def gdtimeMod(tempo, init_tempo):return round(max(0.10, min(2.0, (tempo / init_tempo) * 1.0)), 2)
+def getTimeforMove(length_midi, ticks): return round(length_midi*ticks)
+
+
 
 def midi_to_gmd(midi_file_path, output_file_path):
+    filename = os.path.basename(midi_file_path)
+    print("MIDI Selected: ", filename)
     print("Loading MIDI...")
+    
     midi = MidiFile(midi_file_path)
+    seconds_midi = midi.length
+
+    print(midi.length)
+
     lines = []
-    time_per_tick = 600 / midi.ticks_per_beat
+    tempos = []
+    ticks_ingame_viewer = 600
+    time_per_tick = ticks_ingame_viewer / midi.ticks_per_beat
+    ticks_per_beat_var = midi.ticks_per_beat
+
+    print(time_per_tick)
+    print(ticks_per_beat_var)
+
     initial_y_pos = 1005
+    total_notes = 0
     big_note_y = 0
     note_start_times = {}
+    total_ticks = 0
+    start_time = 0
+    init_tempo = 0
+    start_time_tempo = 0
+    notes = 0
+    notes_in_seconds = [0] * int(midi.length)
 
     print("Generating objects...")
     for track in midi.tracks:
+        track_ticks = 0
         current_time = 0
+
+
         for msg in track:
+            track_ticks += msg.time
+            if msg.type == 'note_on' or msg.type == 'note_off':total_notes += 1
             if msg.type == 'note_on' and msg.velocity > 0:
                 note_start_times[msg.note] = current_time
             elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
                 start_time = note_start_times.get(msg.note, current_time)
+
+                
+                try:notes_in_seconds[int((start_time/ticks_per_beat_var)/2)] += 1
+                except:print("Error asing notes x sec")
                 duration = current_time - start_time
-                scaleX, scaleY = 0.25, max(msg.time * 0.045, 0.1)
-                xPos = 4 + (msg.note * 7.5)
+                scaleX, scaleY = 0.25, max(msg.time * 0.045, max((duration/time_per_tick), 0.05))
+                xPos = (4 + (msg.note * -7.5)+960) #invert Notes view
                 yPos = initial_y_pos + (start_time * time_per_tick) + ((30 * scaleY - 30) / 2)
                 big_note_y = max(big_note_y, yPos)
                 colorChannel = (msg.channel - 1) % 15 + 1
@@ -31,9 +69,49 @@ def midi_to_gmd(midi_file_path, output_file_path):
                 line = (f"1,890,2,{yPos:.2f},3,{xPos},57,0,21,{colorChannel},32,1.0,155,1,128,{scaleY:.2f},129,{scaleX:.2f},"
                         f"24,{soundChannel},25,{soundChannel};")
                 lines.append(line)
-            current_time += msg.time
+            elif msg.type == 'set_tempo':
+                bpm = 60_000_000 / (msg.tempo * ticks_per_beat_var)
+                modTime = bpm * 10
+                start_time_tempo+=msg.time 
+                print("Tempo start:", start_time_tempo)
+                posX = initial_y_pos + (start_time_tempo * time_per_tick)
 
+                if(init_tempo == 0):
+                    init_tempo = modTime
+                    lines.append(f"1,1616,2,{posX},3,527.25,51,10,580,0,11,1;")
+                    tempos.append(f"1,901,2,{posX},3,527.25,155,1,36,1,51,1,57,10,11,1,28,REPLACE_TXT,29,0,10,{seconds_midi:.4f},30,0,85,2;")
+                print("Mod time:", modTime, " in X:", posX)
+                print(start_time)
+
+                ## Timewarp
+                timeMod = gdtimeMod(modTime,init_tempo)
+                lines.append(f"1,1935,2,{posX},3,527.25,155,1,13,1,36,1,120,{timeMod},11,1;")
+
+                
+            current_time += msg.time
+        total_ticks += track_ticks
+
+
+    for tempo in tempos:
+        final = int(big_note_y+initial_y_pos)
+        lines.append(tempo.replace("REPLACE_TXT", str( (final) ) ) )
+    XSum = ((big_note_y-initial_y_pos) / seconds_midi)
+    for pos, notes_count in enumerate(notes_in_seconds, start=1):
+        xpos_counter = (XSum * pos) +  initial_y_pos
+        ## Counter notes
+        lines.append(f"1,1817,2,{xpos_counter:.2f},3,527.25,155,1,11,1,36,1,80,3,77,{int(notes_count)},449,1;1,1916,2,-45,3,-195,155,1,36,1,85,2;")
+        
+    ## Background Color
     lines.append("1,899,2,-15,3,-15,155,1,36,1,7,48,8,48,9,48,10,0,35,1,23,1000;")
+    ## Move
+    lines.append(f"1,901,2,12.5,3,-73.75,155,1,36,1,51,1,57,10,28,{str(int(initial_y_pos+30))},29,0,10,1.3,30,0,85,2;")
+    ## Text MIDI Name
+    lines.append(f"1,914,2,213.316,3,1866.82,57,3,155,4,21,25,128,0.25,129,0.25,31,{gdText(filename)};")
+    ## Text Notes
+    lines.append(f"1,914,2,213.316,3,1914.82,57,3,155,4,21,25,128,0.25,129,0.25,31,{gdText('Total Notes: {}'.format(int(total_notes/2)))};")
+
+
+
     colors = [
         (51, 102, 255), (255, 126, 51), (51, 255, 102), (255, 51, 129),
         (51, 255, 255), (228, 51, 255), (153, 255, 51), (75, 51, 255),
@@ -44,8 +122,8 @@ def midi_to_gmd(midi_file_path, output_file_path):
 
     lvlstr = '\n'.join(lines)
 
-    with open("var.txt", "r") as v:
-        lvlstr = v.read() + lvlstr
+    with open("var.txt", "r") as v:lvlstr = v.read() + lvlstr
+    with open("ui.txt", "r") as v:lvlstr = v.read() + lvlstr
 
     compressed_lvlstr = compress(lvlstr.encode())
     encoded_lvlstr = urlsafe_b64encode(compressed_lvlstr).decode()
@@ -58,6 +136,7 @@ def midi_to_gmd(midi_file_path, output_file_path):
     with open(output_file_path, 'wb') as fn:
         fn.write(final.encode())
 
+    print("X: ", big_note_y)
     print("Finished")
 
 def select_midi_file():
